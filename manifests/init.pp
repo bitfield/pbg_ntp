df0cd43bbf3909e0742baef196f55943dd7164d6
John Arundel <john@bitfieldconsulting.com>
2017-08-10 12:36:12 +0100

    Update file source

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ class pbg_ntp {
   ensure_packages(['ntp'])
 
   file { '/etc/ntp.conf':
-    source  => 'puppet:///modules/ntp/ntp.conf',
+    source  => 'puppet:///modules/pbg_ntp/ntp.conf',
     notify  => Service['ntp'],
     require => Package['ntp'],
   }
